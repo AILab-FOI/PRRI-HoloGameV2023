c7ec78676e4f8d3d34d4a4bfde37eb648f7e74ab
Noa Midzic <nmidzic@yahoo.com>
2023-05-02 11:56:44 +0200

    Tested motion sensor alerting camera to start

--- GamePad/server.py
+++ GamePad/server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+ 
 import flask
 from flask import Flask, render_template, send_from_directory, request, abort
 from flask_socketio import SocketIO, emit
@@ -10,40 +10,42 @@ import threading
 from threading import Thread
 import time
 import random
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
+GPIO.setmode( GPIO.BCM )
 PIR_PIN = 21
-
+GPIO.setup( PIR_PIN, GPIO.IN )
+ 
 from config import GAMES
-
+ 
 GAME = GAMES[ 'abe' ] # change this when multiple games are available
-
+ 
 ADDR_OUT = '0.0.0.0'
 PORT = 5000
-
-app = Flask(__name__)
+ 
+app = Flask(_name_)
 app.config[ 'SECRET_KEY' ] = 'šekret'
 socketio = SocketIO( app, cors_allowed_origins="*", async_mode='eventlet')
 import eventlet
 eventlet.monkey_patch()
-
+ 
 PLAYERS = 0
 GAME_STARTED = False
-
+ 
 cameraSignal = False
-
+ 
 # A set to keep track of connected clients
 connected_clients = set()
-
+ 
 def popenAndCall( onExit, *popenArgs ):
     """
     Runs the given args in a subprocess.Popen, and then calls the function
     onExit when the subprocess completes.
     onExit is a callable object, and popenArgs is a list/tuple of args that 
     would give to subprocess.Popen.
-    
+ 
     Adapted from: http://stackoverflow.com/questions/2581817/python-subprocess-callback-when-cmd-exits
     """
-
+ 
     def runInThread( onExit, *popenArgs ):
         proc = subprocess.Popen( *popenArgs )
         proc.wait()
@@ -53,41 +55,40 @@ def popenAndCall( onExit, *popenArgs ):
     thread.start()
     # returns immediately after the thread starts
     return thread
-
+ 
 def background_thread():
     while True:
         global count
         time.sleep(5)
-        motiondetect() # Remove this when testing with a real motion sensor
         if sendSignal() == True:
             global cameraSignal
             print("Emitting for camera..." + str(cameraSignal))
-            socketio.emit('startCamera', {'message': 'Server generated event'}, broadcast=True, namespace='/')
+            socketio.emit('startCamera', {'message': 'Server generated event'}, namespace='/')
             cameraSignal = False
-
+ 
 def sendSignal():
     return cameraSignal
-    
-def motiondetect():
+ 
+def motiondetect(test=None):
     global cameraSignal
     if (cameraSignal == False):
         cameraSignal = True
-
+ 
 @socketio.on( 'connect' )
 def connect():
     print( 'Client connected:', request.sid )
     connected_clients.add( request.sid )
     global PLAYERS
     PLAYERS += 1
-
-
+ 
+ 
 @socketio.on( 'disconnect' )
 def disconnect():
     print( 'Client disconnected:', request.sid )
     connected_clients.remove( request.sid )
     global PLAYERS
     PLAYERS -= 1
-
+ 
 @socketio.on( 'ctrl' )
 def handle_message( message ):
     print( 'ctrl', message )
@@ -116,7 +117,7 @@ def handle_message( message ):
         print( "Error, unknown game! Player", PLAYERS )
         emit( 'error', {"message": "Error! Unknown game!" }, broadcast=True )
         return
-    
+ 
     if cmd in GAMES[ game ][ 'taps' ]:
         if context == "start":
             pyautogui.press( toggles[ cmd ] ) # discard stop, only one tap is needed
@@ -126,8 +127,8 @@ def handle_message( message ):
         else:
             pyautogui.keyUp( toggles[ cmd ] )
     print( 'Player', PLAYERS, 'Got', cmd, context )
-
-
+ 
+ 
 # Route for serving the controller
 @app.route( '/' )
 def ctrl():
@@ -140,28 +141,27 @@ def ctrl():
     popenAndCall( lambda: game_exit_callback(), GAME[ 'executable' ] )
     GAME_STARTED = True  
     return render_template( 'ctrl.html', game='abe' )
-
-
+ 
+ 
 # Route for serving the start button
 @app.route( '/start' )
 def start():
-    GPIO.setup(PIR_PIN, GPIO.IN)
-    GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=motiondetect)
     Cthread = Thread(target=background_thread)
     Cthread.daemon = True
     Cthread.start()
     return render_template( 'index.html' )
-
+ 
 # Routes for serving static files
 @app.route( '/images/<path:path>' )
 def serve_images( path ):
     return send_from_directory( 'images', path )
-
+ 
 @app.route( '/js/<path:path>' )
 def serve_js( path ):
     return send_from_directory( 'js', path )
-
-
-if __name__ == '__main__':
+ 
+ 
+if _name_ == '_main_':
     print( 'Starting server ...' )
+    GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=motiondetect)
     socketio.run( app, host='0.0.0.0', port=5000 )
